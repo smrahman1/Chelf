9f367d12c10e988d0f097d884965975247b97b04
Sheikh Mahfuzur Rahman <mahfuzur@Mahfuzurs-MacBook-Air.local>
2023-01-29 08:51:32 -0500

    generate page updates

--- frontend/src/components/Generate.js
+++ frontend/src/components/Generate.js
@@ -1,5 +1,42 @@
+import "./Generate.css";
+import LoopIcon from "@mui/icons-material/Loop";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import RecipeItem from "./RecipeItem";
+
 function Generate() {
-  return <div>Generate</div>;
+  const testData = [
+    {
+      title: "Chicken Parmesan",
+    },
+    {
+      title: "Chick Parm",
+    },
+    {
+      title: "Parm",
+    },
+  ];
+
+  return (
+    <>
+      <div className="container3">
+        <div className="title">Generate Recipes</div>
+        <LoopIcon className="cycleIcon" />
+      </div>
+
+      <div className="container4">
+        <FilterAltOutlinedIcon className="filterIcon" />
+        <div className="filters">Add Filters</div>
+        <div className="spacer"></div>
+        <ArrowForwardIosOutlinedIcon className="arrowIcon" />
+      </div>
+
+      <div className="recipesContainer">
+        {testData && testData.map((item) => <RecipeItem item={item} />)}
+        {!testData && <div className="noRecipes">No Recipes Found</div>}
+      </div>
+    </>
+  );
 }
 
 export default Generate;
---
+++ frontend/src/components/RecipeItem.js
@@ -0,0 +1,15 @@
+import "./RecipeItem.css";
+import { GiHotMeal } from "react-icons/gi";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+
+function RecipeItem(props) {
+  return (
+    <div className="recipe">
+      <GiHotMeal className="recipeIcon" />
+      <h2 className="recipeTitle">{props.item.title}</h2>
+      <VisibilityIcon className="viewIcon" />
+    </div>
+  );
+}
+
+export default RecipeItem;
