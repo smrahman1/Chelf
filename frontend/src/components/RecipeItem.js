ee1010095f5e46120c5e09019830565add4da93d
Abdullah Abdullah <a55abdul@uwaterloo.ca>
2023-01-29 11:14:49 -0500

    removed share icons

--- frontend/src/components/RecipeItem.js
+++ frontend/src/components/RecipeItem.js
@@ -10,40 +10,8 @@ function RecipeItem(props) {
         <h2 className="recipeTitle">{props.item.Name}</h2>
         <VisibilityIcon className="viewIcon" />
       </div>
-      <div id="share-buttons">
-
-        <a class="facebook" target="blank"><i class="fab fa-facebook"></i></a>
-      
-        <a class="twitter" target="blank"><i class="fab fa-twitter"></i></a>
-        
-        <a class="linkedin" target="blank"><i class="fab fa-linkedin"></i></a>
-        
-        <a class="reddit" target="blank"><i class="fab fa-reddit"></i></a>
-
-        <a class="instagram" target="blank"><i class="fab fa-instagram"></i></a>
-
-      </div>
     </div>
   );
 }
-
-const link = encodeURI();
-const msg = encodeURIComponent('Hey, I found this awesome recipe!');
-const title = encodeURIComponent(document.querySelector('.recipeTitle'));
-
-const fb = document.querySelector('.facebook');
-fb.href = `https://www.facebook.com/share.php?u=${link}`;
-
-const twitter = document.querySelector('.twitter');
-twitter.href = `http://twitter.com/share?&url=${link}&text=${msg}&hashtags=javascript,programming`;
-
-const linkedIn = document.querySelector('.linkedin');
-linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
-
-const reddit = document.querySelector('.reddit');
-reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
-
-const instagram = document.querySelector('.instagram');
-instagram.href = `http://www.instagram.com/submit?url=${link}&title=${title}`;
 
 export default RecipeItem;
